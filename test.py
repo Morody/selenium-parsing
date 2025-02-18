d075b84dca4bc9cd735b82ccf52956df6e1bfbfc
Morody <adel-ufa@mail.ru>
2025-02-18 13:08:08 +0300

    Changed counts and if start_page

--- test.py
+++ test.py
@@ -1,10 +1,2 @@
-﻿import re
-import requests
-import json
-import pandas as pd
-import math
-
-print(math.ceil(190 / 50))
-
-
- 
+Mydict = {"statusCode":403,"error":"Forbidden","message":"Limit is exceeded"}
+print(Mydict['message'])
