fdb929c1467b03316d0ce3d562cbd06463cfa146
Morody <adel-ufa@mail.ru>
2025-02-18 07:09:12 +0300

    'fin'

--- avito_parser.py
+++ avito_parser.py
@@ -10,64 +10,97 @@ warnings.filterwarnings('ignore')
 import time
 import re
 import math
+import ischedule
+from datetime import datetime
 
-options = webdriver.ChromeOptions()
-
-options.add_argument("--start-maximized")
-
-driver = webdriver.Chrome(options=options)
-url = 'https://www.avito.ru/kazan/kvartiry/prodam/novostroyka-ASgBAgICAkSSA8YQ5geOUg?context=H4sIAAAAAAAA_wEtANL_YToxOntzOjg6ImZyb21QYWdlIjtzOjE2OiJzZWFyY2hGb3JtV2lkZ2V0Ijt9F_yIfi0AAAA'
-
-str = 'https://geocode-maps.yandex.ru/1.x/?apikey=8a764659-98c0-4eb9-8683-ef1591e7db86&geocode=Дубай, бульвар Мухаммед Бин Рашид, дом 1&format=json'
-
-driver.get(url)
-
-res_table = pd.DataFrame({
-    'Цена за м2' : [],
-    'Координаты' : []
-})
-
-ads_count = driver.find_element(By.XPATH, "//span[@data-marker='page-title/count']").text.replace(' ','') # количество объявлений
-
-page_count = driver.find_elements(By.XPATH, "//nav[@aria-label='Пагинация']/ul/li")[7]
-df = pd.read_excel('data.xlsx')
-
-num_curr_recs = df.shape[0]
-start_page = math.ceil(num_curr_recs / 50) if (num_curr_recs // 50) != 0 else 1
-
-for page in range(start_page, start_page + 1): # увеличивать на 20
-    driver.get(f'{url}&p{page}')
-    driver.implicitly_wait(3)    
-    ads_elements = driver.find_elements(by=By.XPATH, 
-                                        value='//div[@data-marker="item"]')
-    for ad in ads_elements:
-        print(ad.text)
-        priceSquare = re.search(r'[ \d]+ ₽ за м²', ad.text)
-        address = re.search(r'ул. [А-Яа-яёЁ0-9,.\-\/ ]+', ad.text)
-
-        if (address is None):
-            pass
-        else:
-            response = requests.get(f'https://geocode-maps.yandex.ru/1.x/?apikey=8a764659-98c0-4eb9-8683-ef1591e7db86&geocode=Казань, {address[0]}, &format=json')
-
-            InBetweenRes = re.search(r'pos[\d\"\.\: ]+}', response.text)
-            ListPos = re.search(r'[\d\. ]+', InBetweenRes[0])[0].split(' ')
-
-            temp_data = pd.DataFrame({
-            'Цена за м2' : [priceSquare[0]],
-            'Координаты' : [f'{ListPos[1]} {ListPos[0]}']
-            })
-
-            res_table = res_table._append(temp_data)
+def myFunc():
+    options = webdriver.ChromeOptions()
+    
+    options.add_argument("--no-sandbox")
+    options.add_argument("--headless")
+    options.add_argument("--disable-dev-shm-usage")
+    
+    
+    driver = webdriver.Chrome(options=options)
+    url = '''https://www.avito.ru/kazan/kvartiry/prodam/novostroyka-ASgBAgICAkSSA8YQ5geOUg?
+                context=H4sIAAAAAAAA_wEtANL_YToxOntzOjg6ImZyb21QYWdlIjtzOjE2OiJzZWFyY2hGb3JtV2lkZ2V0Ijt9F_yIfi0AAAA'''
+    
+    
+    driver.get(url)
+    
+    res_table = pd.DataFrame({
+        'Цена за м2' : [],
+        'Координаты' : []
+    })
+    
+    ads_count = driver.find_element(By.XPATH, "//span[@data-marker='page-title/count']").text.replace(' ','') # текущее количество страниц с объявлениями по новостройкам на Авито
+    
+    page_count = driver.find_elements(By.XPATH, "//nav[@aria-label='Пагинация']/ul/li")[7]
 
 
-    print(f"Страница {page} готова")
 
-df_finally_res = pd.concat([
-        df,
-        res_table
-])
-df_finally_res.to_excel('data.xlsx', index=False)
+    excel_reader = pd.ExcelFile('tables/data.xlsx')
 
+    df_dict = {}
 
-driver.close()
+    for sheet_name in excel_reader.sheet_names:
+	    df_dict[sheet_name] = excel_reader.parse(sheet_name)
+
+    sheet_name = str(datetime.now().date()) # представляем в виде строки дату на данный момент - это будет название листа, в который мы запишем актуальные данные на сегодня
+
+    num_curr_recs = 0 # количество записей уже существующих в таблице
+
+    for i in df_dict.keys():
+    	num_curr_recs += df_dict[i].shape[0]
+
+    start_page = math.ceil(num_curr_recs / 50) if (num_curr_recs // 50) != 0 else 1 # количество объявлений уже записанных в таблице делим на 50 тем самым определяем стартовую страницу для сбора данных на текущей итерации
+    
+    for page in range(start_page, start_page + 20): # увеличивать на 20, т.к есть ограничение на запросы ~1000, 1 страница с Авито = 50 объявлений, 20 стр. = ~1000
+        count = 0 # счетчик количества собранных объявлений со страницы
+        driver.get(f'{url}&p{page}')
+        driver.implicitly_wait(3)    
+        ads_elements = driver.find_elements(by=By.XPATH, 
+                                            value='//div[@data-marker="item"]')
+        for ad in ads_elements:
+            
+            priceSquare = re.search(r'[ \d]+ ₽ за м²', ad.text)
+            address = re.search(r'(?:ул. [А-Яа-яёЁ0-9,.\-\/ ]+|пр-т [А-Яа-яёЁ0-9,.\-\/ ]+)', ad.text)
+            if (address is None):
+                pass
+            else:
+                response = requests.get(f'https://geocode-maps.yandex.ru/1.x/?apikey=8a764659-98c0-4eb9-8683-ef1591e7db86&geocode=Казань, {address[0]}, &format=json')
+                count += 1
+                InBetweenRes = re.search(r'pos[\d\"\.\: ]+}', response.text)
+                ListPos = re.search(r'[\d\. ]+', InBetweenRes[0])[0].split(' ')
+    
+                temp_data = pd.DataFrame({
+                'Цена за м2' : [priceSquare[0]],
+                'Координаты' : [f'{ListPos[1]} {ListPos[0]}']
+                })
+    
+                res_table = res_table._append(temp_data)
+    
+    
+        print(f"Страница {page} готова, количество собранных объявлений:{count}")
+     
+    if (sheet_name in df_dict.keys()):
+    	fin_data = pd.concat([
+    		df_dict[sheet_name],
+    		res_table
+    	])
+    	df_dict[sheet_name] = fin_data
+    else:
+    	df_dict[sheet_name] = res_table
+    
+    
+    with pd.ExcelWriter(
+    					'tables/data.xlsx',
+    					engine='xlsxwriter',
+    					mode='w') as excel_writer:
+    						for sheet_name in df_dict.keys():
+    							df_dict[sheet_name].to_excel(excel_writer, sheet_name=sheet_name, index=False)
+         
+    driver.close()
+
+ischedule.schedule(myFunc, interval=60*60*24) # сутки в секундах 60*60*24
+ischedule.run_loop()
